2c2b60403e1ae4028f5f9b5386003bbadfbe8acd
Iskander Ignatev <iskander.ignatev@gmail.com>
2022-05-21 11:06:25 +0400

    init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script>
+import FileViewer from './components/FileViewer.vue'
+
+export default {
+  components: {
+    FileViewer
+  },
+  data() {
+    return {
+      catalog: {
+    "name": "lev",
+    "children": [
+        {
+            "name": "file-1.txt",
+            "children": []
+        },
+        {
+            "name": "file-2.bin",
+            "children": []
+        }
+    ]
+}
+    }
+  }
+}
+</script>
+
+<template>
+  <file-viewer :catalog="catalog"></file-viewer>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/FileViewer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>{{ catalog.name }}</div>
+</template>
+
+<script>
+export default {
+    name: "FileViewer",
+    props: {
+        catalog: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
